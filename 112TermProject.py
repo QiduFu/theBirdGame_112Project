23bea6180940ca2dd6b30d76c90862b034f64dfc
Qidu Fu <qidufu16@gmail.com>
2023-04-06 16:07:37 -0400

    firstCommitt

---
+++ 112TermProject.py
@@ -0,0 +1,136 @@
+#BEE GAME
+    #date: 04/03/2023
+    #spring23 15112 term project
+    #name: Qidu Fu
+    #andrewId: qiduf
+    #email: qiduf@andrew.cmu.edu
+# --------------------------------------------------------------------------
+
+# inspired bu the Google's Earth Day Doodle and Mike's scaffolded project
+# https://www.google.com/doodles/earth-day-2020
+
+from cmu_graphics import *
+import random
+import math, time
+
+
+# The BEE (helper BEES)
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+
+class Bee(object):
+    def __init__(self, app):
+        pass
+    
+    def onAppstep(self):
+        pass
+
+    def getTarget(Self):
+        pass
+
+    def getHeldColors(self):
+        pass
+
+    def gather(self, other):
+        pass
+
+    def redraw(self, canvas):
+        pass
+
+# The BEE (Player BEES)
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+
+class Player(Bee):
+    def __init__(self, app):
+        pass
+    
+    def onAppstep(self):
+        pass
+
+    def getTarget(Self):
+        pass
+
+    def getHeldColors(self):
+        pass
+
+    def gather(self, other):
+        pass
+
+    def redraw(self, canvas):
+        pass
+
+
+# The BEE (Player BEES)
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+
+class Player(Bee):
+    def __init__(self, app):
+        pass
+    
+    def onAppstep(self):
+        pass
+
+        pass
+
+    def redrawll(self, canvas):
+        super().redraw(canvas)
+        pass
+
+# FLOWER
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+
+class Flower(object):
+
+    @staticmethod
+    def update():
+        pass
+
+    def __init__(self, app):
+        pass
+    
+    def onAppstep(self):
+        pass
+
+    def inView(self):
+        pass
+
+    def gather(self, got):
+        pass
+
+    def redraw(self, x=None, y=None):
+        pass
+
+
+# APP
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+
+def onAppStart(app):
+    pass
+
+def onMouseMove(app):
+    pass
+
+def onAppStep(app):
+    pass
+
+def redrawAll(app):
+    pass
+
+def main():
+    runApp(width=800, height=800, title='Bee Game!')
+
+main()
